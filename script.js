4eccea4976d66e254605d77cf6a3c1ecf3c47f5c
Jules Baclet <jules.baclet@efrei.net>
2024-09-11 16:48:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("btn")
+const st = document.getElementById("titre")
+function click(){
+    window.location.href = "introduction.html";
+}
+
+btn.addEventListener("click",click);
+
+function click(){
+    st.innerText = "ta mere la pute"
+}
